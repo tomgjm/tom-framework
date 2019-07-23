97647cbaadcb8983607f533435846faac2b9a58b
tiandiwy <tiandiwy@aliyun.com>
2019-07-23 15:37:05 +0800

    startRun函数会返回app,server_http,server_https三个对象方便测试

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require2('tomjs/handlers/log4js'); //日志和控制台输出处理
 const SystemConfig = require2('tomjs/configs')().system;
 const startRun = require2('tomjs/app');
 
-startRun().then(app => {
+startRun().then(({ app, server_http, server_https }={}) => {
     if (SystemConfig.server_run_type_https) {
         console.log('Now start https server on IP:' + SystemConfig.server_bind_ip + ':' + SystemConfig.server_https_port + '...');
     }
