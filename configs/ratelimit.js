05d1b383eb8eb91f37835a589620199bdb28c2c7
tiandiwy <tiandiwy@aliyun.com>
2019-07-29 13:23:41 +0800

    让ratelimit同时支持本机内存和redis

--- configs/ratelimit.js
+++ configs/ratelimit.js
@@ -1,6 +1,6 @@
 module.exports = {
     default: {
-        redis_cfg: process.env.RATELIMIT_DEFAULT_REDIS_URL || "redis://127.0.0.1:6379/0", //"redis://:authpassword@127.0.0.1:6380/4"
+        redis_cfg: process.env.RATELIMIT_DEFAULT_REDIS_URL || "redis://127.0.0.1:6379/0",
         //redis_cfg 也可以如下配置
         // redis_cfg:{
         //     port: 6379,          // Redis port
@@ -12,7 +12,9 @@ module.exports = {
         options: {
             duration: process.env.RATELIMIT_DEFAULT_DURATION || 60000,//时间范围 单位毫秒 也可以用字符串 1d：一天 1h：一小时 1m：一分钟 来表示
             errorMessage: 'too many requests',
-            id: (ctx) => ctx.ip + ' ' + ctx.path,
+            //prefixKey: 'default',
+            //or
+            //id: (ctx) => 'default '+ ctx.ip,
             headers: {
                 remaining: 'Rate-Limit-Remaining',
                 reset: 'Rate-Limit-Reset',
@@ -20,17 +22,17 @@ module.exports = {
             },
             max: process.env.RATELIMIT_DEFAULT_MAX || 10,
             disableHeader: false,
-            throw: true,
+            show_api_error: true,
         },
         whitelist: ["::1", "127.0.0.1"],//白名单 支持 数组，字符串（可用,分割）以及函数 参数有ctx,ip 返回 true或false 函数可以是 async 函数
         blacklist: [],//黑名单 支持 数组，字符串（可用,分割）以及函数 参数有ctx,ip 返回 true或false 函数可以是 async 函数（黑名单优先于白名单）
+        blackMessage: 'This is forbidden area for you.',
     },
     mobile: {
-        //不设置 redis_url 参数就直接只用default的redis_cfg的redis_url参数进行连接并且所有没有redis_url参数配置都公共一个redis连接 否则独立开设新的redis连接
+        //不设置 redis_url 就表示使用本机内存
         options: {
             duration: process.env.RATELIMIT_MOBILE_DURATION || '1m',
             errorMessage: 'too many requests',
-            id: (ctx) => ctx.ip + ' ' + ctx.path,
             headers: {
                 remaining: 'Rate-Limit-Remaining',
                 reset: 'Rate-Limit-Reset',
@@ -38,17 +40,17 @@ module.exports = {
             },
             max: process.env.RATELIMIT_MOBILE_MAX || 1,
             disableHeader: false,
-            throw: true,
+            show_api_error: false,
         },
-        whitelist: ["::1", "127.0.0.1"],
+        //whitelist: ["::1", "127.0.0.1"],
         blacklist: [],
+        blackMessage: 'This is forbidden area for you.',
     },
     login: {
-        //不设置 redis_url 参数就直接只用default的redis_cfg的redis_url参数进行连接并且所有没有redis_url参数配置都公共一个redis连接 否则独立开设新的redis连接
+        //不设置 redis_url 就表示使用本机内存
         options: {
             duration: process.env.RATELIMIT_LOGIN_DURATION || '1m',
             errorMessage: 'too many requests',
-            id: (ctx) => ctx.ip + ' ' + ctx.path,
             headers: {
                 remaining: 'Rate-Limit-Remaining',
                 reset: 'Rate-Limit-Reset',
@@ -56,9 +58,10 @@ module.exports = {
             },
             max: process.env.RATELIMIT_LOGIN_MAX || 5,
             disableHeader: false,
-            throw: true,
+            show_api_error: true,
         },
-        whitelist: ["::1", "127.0.0.1"],
+        //whitelist: ["::1", "127.0.0.1"],
         blacklist: [],
+        blackMessage: 'This is forbidden area for you.',
     },
 };
