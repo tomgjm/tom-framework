3fc8f2151fb586553797eabf2fa440d7f1342e9a
tiandiwy <tiandiwy@aliyun.com>
2019-08-21 15:19:28 +0800

    完善 websocket-router.js 客户端 websocket_client.js 支持 sendAsync 方法

--- src/websocket/routes/socket/api.js
+++ src/websocket/routes/socket/api.js
@@ -1,9 +1,11 @@
 const require2 = require('tomjs/handlers/require2');//可以开始使用require2引入模块了
 const Router = require2('koa-router');
+const BaseApiError = require2('tomjs/error/base_api_error');
 const authorize = require2('tomjs/handlers/authorize');//数据库模型权限验证函数
 const validator = require2('tomjs/handlers/validator');//表单验证 validator(ctx,'websocket/a@message',{});
 const { login, retoken, decode_token, logout } = require2('tomjs/handlers/login_out');//提供用户登录，登出，生成新的token，解读token等功能
 const ratelimit = require2('tomjs/middleware/ratelimit');//访问限制器
+
 
 let route = new Router();
 route.use('/', ratelimit('websocket').websocket);
--- src/websocket/routes/url/api.js
+++ src/websocket/routes/url/api.js
@@ -3,7 +3,7 @@ const WsRouter = require2('tomjs/router/websocket-router');
 const ratelimit = require2('tomjs/middleware/ratelimit');//访问限制器
 
 let route = new WsRouter();
-route.all('/', async function (ctx, next) {
+route.path('/', async function (ctx, next) {
     ctx.websocket.on_message = async function (data) {
         await ctx.websocket.send('on_message:' + JSON.stringify(data));
     };
@@ -15,5 +15,5 @@ route.all('/', async function (ctx, next) {
     return next();
 });
 
-route.ws_router('/test/:id', 'api');
+route.path('/test/:id', 'api');
 module.exports = route;
