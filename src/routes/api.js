ccb8100bd78d7e5b1e7e20bb24f66373126ca2e9
tiandiwy <tiandiwy@aliyun.com>
2019-09-18 13:05:07 +0800

    1. .evn 添加SERVER_HTTP_PORT、SERVER_HTTPS_PORT 2. api.js 将 v2相关路径代码注释掉 3. 精简 show.js代码

--- src/routes/api.js
+++ src/routes/api.js
@@ -11,10 +11,10 @@ api_v1.get('/users/lang/', 'api/v1/users@getLang');
 api_v1.get('/users/lang/:lang', 'api/v1/users@setLang');
 api_v1.resource('/users/', 'api/v1/users',{name:'users'});
             
-let api_v2 = new MainRouter();
-api_v2.resource('/users/', 'api/v2/users', {'only': 'index,show'});
+// let api_v2 = new MainRouter();
+// api_v2.resource('/users/', 'api/v2/users', {'only': 'index,show'});
 
 let routers = new KoaRouter();
 routers.use('/v1', api_v1.routes(), api_v1.allowedMethods())
-    .use('/v2', api_v2.routes(), api_v2.allowedMethods());
+//    .use('/v2', api_v2.routes(), api_v2.allowedMethods())
 module.exports = routers;
