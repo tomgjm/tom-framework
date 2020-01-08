95506cbfb021e4658b323e496e9b450a29b9406c
tiandiwy <tiandiwy@aliyun.com>
2020-01-08 09:54:12 +0800

    LOG_DEFAULT_PATH无需/结尾

--- configs/log.js
+++ configs/log.js
@@ -1,5 +1,5 @@
 const { toBool } = require('tomjs/handlers/base_tools')
-let LOG_PATH = process.env.LOG_DEFAULT_PATH || "./logs/";
+let LOG_PATH = process.env.LOG_DEFAULT_PATH || "./logs";
 module.exports = {
     open_koa_logger: toBool(process.env.LOG_OPEN_KOA_LOGGER || true),
     open_log4js: toBool(process.env.LOG_OPEN_LOG4JS || true),
@@ -12,43 +12,43 @@ module.exports = {
             },
             "default": {
                 "type": "dateFile",
-                "filename": process.env.LOG_DEFAULT_FILE || (LOG_PATH + "common-all"),
+                "filename": process.env.LOG_DEFAULT_FILE || (LOG_PATH + "/common-all"),
                 "pattern": "_yyyyMMdd.log",
                 "alwaysIncludePattern": true,
             },
             "app": {
                 "type": "dateFile",
-                "filename": process.env.LOG_APP_FILE || (LOG_PATH + "app"),
+                "filename": process.env.LOG_APP_FILE || (LOG_PATH + "/app"),
                 "pattern": "_yyyyMMdd.log",
                 "alwaysIncludePattern": true,
             },
             "sms": {
                 "type": "dateFile",
-                "filename": process.env.LOG_SMS_FILE || (LOG_PATH + "sms"),
+                "filename": process.env.LOG_SMS_FILE || (LOG_PATH + "/sms"),
                 "pattern": "_yyyyMMdd.log",
                 "alwaysIncludePattern": true,
             },
             "email": {
                 "type": "dateFile",
-                "filename": process.env.LOG_EMAIL_FILE || (LOG_PATH + "email"),
+                "filename": process.env.LOG_EMAIL_FILE || (LOG_PATH + "/email"),
                 "pattern": "_yyyyMMdd.log",
                 "alwaysIncludePattern": true,
             },
             "user": {
                 "type": "dateFile",
-                "filename": process.env.LOG_USER_FILE || (LOG_PATH + "user"),
+                "filename": process.env.LOG_USER_FILE || (LOG_PATH + "/user"),
                 "pattern": "_yyyyMMdd.log",
                 "alwaysIncludePattern": true,
             },
             "getweb": {
                 "type": "dateFile",
-                "filename": process.env.LOG_GETWEB_FILE || (LOG_PATH + "getweb"),
+                "filename": process.env.LOG_GETWEB_FILE || (LOG_PATH + "/getweb"),
                 "pattern": "_yyyyMMdd.log",
                 "alwaysIncludePattern": true,
             },
             "error": {
                 "type": "dateFile",
-                "filename": process.env.LOG_ERROR_FILE || (LOG_PATH + "error"),
+                "filename": process.env.LOG_ERROR_FILE || (LOG_PATH + "/error"),
                 "pattern": "_yyyyMMdd.log",
                 "alwaysIncludePattern": true,
             },
