c2b9998e474e4ccea4e3fbfacc179fe6ac9168ff
tiandiwy <tiandiwy@aliyun.com>
2021-05-24 09:38:02 +0800

    修复一个命名Bug

--- src/controllers/api/v1/auth/password.js
+++ src/controllers/api/v1/auth/password.js
@@ -1,9 +1,9 @@
 const require2 = require('tomjs/handlers/require2');
 const BasePassword = require2('tomjs/controllers/auth/base_password');
 
-class Passwor extends BasePassword {
+class Password extends BasePassword {
     async resetpassword(ctx) {
         await super.resetpassword(ctx);
     }
 }
-module.exports = Passwor
+module.exports = Password
